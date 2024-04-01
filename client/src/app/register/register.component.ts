4389782499a8fa56a28d19f38cbab0b11cd3bd65
mfbernasol <mfbernasol@gmail.com>
2024-04-01 16:21:49 -0500

    created client login and register

---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,29 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css'],
+})
+export class RegisterComponent {
+  @Output() cancelRegister = new EventEmitter();
+  model: any = {};
+  // user: any;
+
+  constructor(private accounService: AccountService){}
+
+  register(){
+    this.accounService.register(this.model).subscribe({
+      next: response => {
+        console.log(response)
+        this.cancel();
+      },
+      error: error => console.log(error)
+    })
+  }
+
+  cancel(){
+   this.cancelRegister.emit(false);
+  }
+}
